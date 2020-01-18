e60ce0cd1542304ba530b59f6a75edd3dbe7da95
Ivan Balashov <iprogrammer52@gmail.com>
2020-01-18 09:59:35 +0400

    Add custom style file, change menu.

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/','IndexController');
 
 Auth::routes();
 
