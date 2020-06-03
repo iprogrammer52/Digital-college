1447ae6632d5428cb232f1177be3c48237ee9d7a
Ivan Programmer <iprogrammer52@gmail.com>
2020-06-04 00:54:17 +0400

    Fix index.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts/app')
 
 @section('content')
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+{{-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="...">
+            <img src="..." class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+            <img src="..." class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+            <img src="..." class="d-block w-100" alt="...">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -21,5 +21,20 @@
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
+</div> --}}
+<div class="container-fluid">
+    <div class="row">
+        <div class="col d-flex justify-content-center flex-column">
+            <h4 class="h4">
+                Lorem, ipsum dolor.
+            </h4>
+            <p class="text-left">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro voluptatibus libero id ea magni perferendis officia vitae velit, quisquam quas ipsam rem cumque animi, perspiciatis reprehenderit in aspernatur corporis maiores.
+            </p>
+        </div>
+        <div class="col d-flex justify-content-center">
+            <img src="https://s11.stc.all.kpcdn.net/share/i/12/10699928/inx960x640.jpg" class="img-fluid" alt="">
+        </div>
+    </div>
 </div>
 @endsection
